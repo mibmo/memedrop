b32dccebe2dd9d8592189527992ce31f6c4b4499
mib <mib@mib.dev>
2021-03-01 00:47:49 +0100

    Add tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+      'templates/**/*.html.tera',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
